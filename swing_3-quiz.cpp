a2ecde9d9c9b78c045900cc99aa3b5d8f768b660
jiyunBae007 <63540652+jiyunBae007@users.noreply.github.com>
2020-05-26 00:04:25 +0900

    add files

---
+++ swing_3-quiz.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int max(int x, int y) {
+	if (x > y) return x;
+	else return y;
+}
+
+int min(int x, int y) {
+	if (x > y) return y;
+	else return x;
+}
+
+int main() {
+	int x, y;
+	cout << "두 수를 입력해 주세요 (띄어쓰기로 구분)<< ";
+	cin >> x >> y;
+	cout << endl;
+
+	cout << "큰 수 >>" << max(x, y) << endl;
+	cout << "작은 수 >>" << min(x, y) << endl;
+}
